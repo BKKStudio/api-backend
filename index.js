86f05e100895e599f5e23ba67a6127fff5021513
First Last <seksak22180@gmail.com>
2024-05-12 20:07:24 +0700

    Add port .env

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const ProductRoutes = require("./src/product/routes")
 const UsersRoutes = require("./src/users/routes")
 const app = express();
-const port = 3000;
+
 
 app.use(express.json())
 
@@ -16,4 +16,4 @@ app.use("/api/products",ProductRoutes)
 
 app.use("/api/users",UsersRoutes)
 
-app.listen(port,() => console.log(`app on port ${port}`))
+app.listen(process.env.PORT,() => console.log(`app on port ${process.env.PORT}`))
