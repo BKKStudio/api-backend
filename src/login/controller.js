7222d79a59753ee00f2f72c9117feb0186e0b334
First Last <seksak22180@gmail.com>
2024-05-14 03:17:33 +0700

    cookie

--- src/login/controller.js
+++ src/login/controller.js
@@ -3,57 +3,40 @@ const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const getlogin = async (req, res) => {
-  
-  try {
-    const { username, password } = await req.body;
-    const {rows} = await sql`SELECT * FROM allusers WHERE username = ${username};`;
-    const user = rows
-    if (!user || !(await bcryptjs.compare(password, user[0].password))) {
-      return res.json({
-          error: "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง",
-      }, { status: 400 });
-  }
-
-    // Create token data
-    const tokenUser = {
-      id: user.id,
-      username: user.username,
-      isadmin: user.isadmin,
-    };
-
-    // Create JWT token
-    const token = jwt.sign(tokenUser, process.env.TOKEN_SECRET);
-
-    // Set token as a cookie
-    const response = res.json({
-      message: "เข้าสู่ระบบสำเร็จ",
-      success: true,
-    });
-    response.cookies.set("token", token, { httpOnly: true });
-
-
-    return response;
-  } catch (error) {
-    console.error(error);
-    // Return error response
-    return res.status(500).json({ error: "เกิดข้อผิดพลาดบางอย่าง" });
-  }
-};
-
-const getuser = async (req, res) => {
     try {
-        const token = req.cookies.get("token")?.value||''        
-        
-        const decode = jwt.verify(token,process.env.TOKEN_SECRET)
-        const user = await sql`SELECT * FROM allusers WHERE id = ${decode.id};`;
-        return res.json(user)
-
+      const { username, password } = req.body;
+      const { rows } = await sql`SELECT * FROM allusers WHERE username = ${username};`;
+      const user = rows[0];
+  
+      if (!user || !(await bcryptjs.compare(password, user.password))) {
+        return res.status(400).json({
+          error: "ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง",
+        });
+      }
+  
+      // Create token data
+      const tokenUser = {
+        id: user.id,
+        username: user.username,
+        isadmin: user.isadmin,
+      };
+  
+      // Create JWT token
+      const token = jwt.sign(tokenUser, process.env.TOKEN_SECRET);
+  
+      // Set token as a cookie
+      res.cookie("token", token, { httpOnly: true });
+  
+      return res.json({
+        message: "เข้าสู่ระบบสำเร็จ",
+        success: true,
+      });
     } catch (error) {
-        throw new Error(error.message)
+      console.error(error);
+      // Return error response
+      return res.status(500).json({ error: "เกิดข้อผิดพลาดบางอย่าง" });
     }
   };
   
-
-module.exports = {
-  getlogin,getuser
-};
+  module.exports = { getlogin };
+  
--- src/login/route.js
+++ src/login/route.js
@@ -6,9 +6,6 @@ router.get("/", async (req, res) => {
     const user = await controller.getuser(req, res);
     res.json(user);
 });
-router.post("/", async (req, res) => {
-    const user = await controller.getlogin(req, res);
-    res.json(user);
-});
+router.post("/", controller.getlogin);
 
 module.exports = router;
