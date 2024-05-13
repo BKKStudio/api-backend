b8fad10d30bea33fcf6de7980948f05be0488e30
First Last <seksak22180@gmail.com>
2024-05-13 18:58:45 +0700

    Edit Delete prissma and .env

--- src/product/controller.js
+++ src/product/controller.js
@@ -1,11 +1,6 @@
-const Prisma = require('@prisma/client')
-
-const prisma = new Prisma.PrismaClient();
-
 
 const getAllProduct = async (req,res) => {
-    const product = await prisma.product.findMany({})
-    return product
+
 }
 
 module.exports = {
--- src/users/controller.js
+++ src/users/controller.js
@@ -1,49 +1,26 @@
-const Prisma = require('@prisma/client')
-
-const prisma = new Prisma.PrismaClient();
-
+const {sql} = require("@vercel/postgres")
 
 const getAllUsers = async (req,res) => {
-  await prisma.$connect();
-    const user = await prisma.user.findMany({})
-    return user
+  try {
+    const product =
+    await sql`SELECT * FROM users;`;
+    return product.rows
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const getUserฺById = async (req,res) => {
-  await prisma.$connect();
-    const id = req.params.id;
-    const user = await prisma.user.findUnique({
-        where: {
-          id: id,
-        },
-      })
-    return user
+
 }
 
 
 const addUser = async (req,res) => {
-  await prisma.$connect();
- const { name, username, password,img } = await  req.body;
-
- const newUser = await prisma.user.create({
-  data: {
-    img,
-    name,
-    username,
-    password,
-  },
-});
-return res.send("สมัครสมาชิกเรียบร้อย กรุุณาเข้าสู่ระบบ")
 
 }
 
 const reMoveUser = async (req,res) => {
-  const id = req.params.id;
-  const user = await prisma.user.delete({
-    where: { id: id },
-  })
-  return res.send("ลบเสร็จสิ้น")
- 
+
  }
 module.exports = {
 getAllUsers,getUserฺById,addUser,reMoveUser
