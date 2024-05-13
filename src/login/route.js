7365776f40264e5a376202e922390d2f7a736b12
First Last <seksak22180@gmail.com>
2024-05-14 02:58:02 +0700

    Update route.js

--- src/login/route.js
+++ src/login/route.js
@@ -2,8 +2,10 @@ const { Router } = require("express");
 const controller = require("./controller");
 const router = Router();
 
-router.get("/", controller.getuser)
-
+router.get("/", async (req, res) => {
+    const user = await controller.getuser(req, res);
+    res.json(user);
+});
 router.post("/", async (req, res) => {
     const user = await controller.getlogin(req, res);
     res.json(user);
